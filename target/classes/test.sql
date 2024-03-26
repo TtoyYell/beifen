383938e4da230ddbe3fb5f444e8a94d11a061bc9
root <1255815114@qq.com>
2024-03-26 19:46:01 +0800

    备份

--- target/classes/test.sql
+++ target/classes/test.sql
@@ -1,138 +1,23 @@
-INSERT INTO "public"."trad_local_edition" (
-    "id","created_time","updated_time","updated_by","dispatch_status","deal_style",
-    "ignored","deal_at","assigned_by","accepted_by","assigned_at","assigned_deadline",
-    "deleted", "reviewed","reviewed_by","reviewed_at","supervise","deal_style2",
-    "name","category_id","advertiser_id","channel_id","first_illegal_time","evidence",
-    "level","laws","description","illegal_description","clue","file_path",
-    "tag_ids","sp_tag_ids","create_by","clue_number","clue_type","clue_time",
-    "delay_time","transfer_party","complaint_party","complaint_party_tel",
-    "accepted_department","advertiser","amount","accepted_user",
-    "area_id","is_timeout","is_delay","action_status","user_deadline"
-)
-VALUES
-    (
-        5142,'2023-12-22','2024-01-11 09:38:49',2384,8,0,
-        'f',NULL,2535,2384, '2023-12-22','2024-04-10',
-        'f','t', NULL,NULL,'f',NULL,
-     '健康有医说医', 13,NULL,1,'2023-12-03','[]',
-        1,'["1"]',NULL,'','省局平台派发（登记）',NULL,
-        NULL,NULL, 2535,'2023122204','平台','2023-12-22',
-        NULL,'','杨澜','15091778315',
-     415,NULL,0,2384,
-        1177,'f',NULL,2,NULL
-    );
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2023-12-22 11:23:33', NULL, NULL, 5142, 1, '[]', 2535,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周瑾璇", "id": "2535"}',
-        2384, '{"department_id": "415", "department_name": "秦淮区市场监督管理局", "name": "马珑珈", "id": "2384"}', '', 'f', NULL);
+INSERT INTO "public"."trad_edition"
+("created_at", "dispatch_status", "deal_style", "ignored", "deal_at",
+"assigned_by", "accepted_by", "assigned_at", "assigned_deadline", "deleted", "reviewed", "reviewed_by", "reviewed_at",
+"supervise", "deal_style2", "name", "size", "category_id", "type", "advertiser_id", "channel_id", "evidence",
+ "level","banner_id", "laws", "description", "illegal_description", "created_by", "original_edition_id", "is_updated",
+"first_illegal_time", "view_count", "source_id", "start_page", "end_page", "extend_phone", "tag_ids", "sp_tag_ids",
+"assignment_to", "allow_distribute", "reviewed_sp_tag", "production_time", "merge_id", "reviewed_back", "timeout",
+"is_timeout", "is_delay", "action_status", "user_deadline")
+VALUES ('2023-04-13 03:02:01.719764+08', 0, 0, 'f', NULL,
+NULL, NULL, NULL, NULL, 'f', 't', 1852, '2023-04-18 18:13:39.334027+08',
+'f', NULL, '我有奇方馆 1740', '1740', 158, 2, 1, 3213,
+'["b61c3e60-117a-11ed-abac-85b8ea1b6f69.1681326130459.W47772.mp3"]', 0, NULL, '[]', '', '', 1899, 47772, 0,
+'2023-03-15 14:30:28+08', NULL, 'b61c3e60-117a-11ed-abac-85b8ea1b6f69', NULL, NULL, NULL, NULL, '{25}',
+33,'f', 't', '2023-03-15 14:30:28+08', NULL, NULL, NULL,
+'f', NULL, 0, 'f');
 
 
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2024-01-10 15:07:11', NULL, NULL, 5142, 7, '[]', 2535,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周瑾璇", "id": "2535"}',
-        2384, '{"department_id": "415", "department_name": "秦淮区市场监督管理局", "name": "马珑珈", "id": "2384"}', '', 'f', NULL);
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2024-01-11 09:38:49', NULL, NULL, 5142, 8, '[{"url": "http://xszhdmp.qiniuoss.xshz.vip/1704937144330.jpg", "name": "20240111-广告科-省广播-立案表（与前期12315举报：丁诚、王安平等并案处理）.jpg"}]', 2535,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周瑾璇", "id": "2535"}',
-        2384, '{"department_id": "415", "department_name": "秦淮区市场监督管理局", "name": "马珑珈", "id": "2384"}', '', 'f', NULL);
-
-
-INSERT INTO "public"."trad_local_edition" (
-    "id","created_time","updated_time","updated_by","dispatch_status","deal_style",
-    "ignored","deal_at","assigned_by","accepted_by","assigned_at","assigned_deadline",
-    "deleted", "reviewed","reviewed_by","reviewed_at","supervise","deal_style2",
-    "name","category_id","advertiser_id","channel_id","first_illegal_time","evidence",
-    "level","laws","description","illegal_description","clue","file_path",
-    "tag_ids","sp_tag_ids","create_by","clue_number","clue_type","clue_time",
-    "delay_time","transfer_party","complaint_party","complaint_party_tel",
-    "accepted_department","advertiser","amount","accepted_user",
-    "area_id","is_timeout","is_delay","action_status","user_deadline"
-)
-VALUES
-    (
-        5139,'2023-12-22','2024-01-11 09:38:11',2384,8,0,
-        'f',NULL,2535,2384, '2023-12-22','2024-04-10',
-        'f','t', NULL,NULL,'f',NULL,
-        '发酵中药；玉茸口服液', 102,NULL,2,'2023-12-03','[]',
-        1,'["1"]',NULL,'','省局平台派发（登记）',NULL,
-        NULL,NULL, 2535,'2023122202','平台','2023-12-22',
-        NULL,'','杨澜','15091778315',
-        415,NULL,0,2384,
-        1177,'f',NULL,2,NULL
-    );
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2023-12-22 11:15:34', NULL, NULL, 5139, 1, '[{"url": "http://xszhdmp.qiniuoss.xshz.vip/1703214595885.WAV", "name": "1043.WAV"}]', 2535,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周瑾璇", "id": "2535"}',
-        2384, '{"department_id": "415", "department_name": "秦淮区市场监督管理局", "name": "马珑珈", "id": "2384"}', '', 'f', NULL);
-
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2024-01-10 15:06:35', NULL, NULL, 5139, 7, '[]', 2535,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周瑾璇", "id": "2535"}',
-        2384, '{"department_id": "415", "department_name": "秦淮区市场监督管理局", "name": "马珑珈", "id": "2384"}', '', 'f', NULL);
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2024-01-11 09:38:11', NULL, NULL, 5139, 8, '[{"url": "http://xszhdmp.qiniuoss.xshz.vip/1704937103927.jpg", "name": "20240111-广告科-省广播-立案表（与前期12315举报：丁诚、王安平等并案处理）.jpg"}]', 2535,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周瑾璇", "id": "2535"}',
-        2384, '{"department_id": "415", "department_name": "秦淮区市场监督管理局", "name": "马珑珈", "id": "2384"}', '', 'f', NULL);
-
-
-
-
-
-INSERT INTO "public"."trad_local_edition" (
-    "id","created_time","updated_time","updated_by","dispatch_status","deal_style",
-    "ignored","deal_at","assigned_by","accepted_by","assigned_at","assigned_deadline",
-    "deleted", "reviewed","reviewed_by","reviewed_at","supervise","deal_style2",
-    "name","category_id","advertiser_id","channel_id","first_illegal_time","evidence",
-    "level","laws","description","illegal_description","clue","file_path",
-    "tag_ids","sp_tag_ids","create_by","clue_number","clue_type","clue_time",
-    "delay_time","transfer_party","complaint_party","complaint_party_tel",
-    "accepted_department","advertiser","amount","accepted_user",
-    "area_id","is_timeout","is_delay","action_status","user_deadline"
-)
-VALUES
-    (
-        5060,'2023-11-27','2023-12-19 10:41:20',2382,8,0,
-        'f',NULL,2377,2382, '2023-11-27','2024-03-18',
-        'f','t', NULL,NULL,'f',NULL,
-        '', 210,NULL,3,'2023-11-27','[]',
-        1,'["1"]',NULL,'','省局平台派发（登记）',NULL,
-        NULL,NULL, 2377,'2023122202','寄送','2023-11-27',
-        NULL,'','','',
-        403,NULL,0,2382,
-        1176,'f',NULL,2,NULL
-    );
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2023-11-27 08:50:17', NULL, NULL, 5060, 1, '[]', 2377,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周晓杰", "id": "2377"}',
-        2384, '{"department_id": "403", "department_name": "玄武区市场监督管理局", "name": "万华", "id": "2382"}', '', 'f', NULL);
-
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2023-12-11 09:36:58', NULL, NULL, 5060, 7, '[]', 2377,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周晓杰", "id": "2377"}',
-        2384, '{"department_id": "403", "department_name": "玄武区市场监督管理局", "name": "万华", "id": "2382"}', '', 'f', NULL);
-
-INSERT INTO "public"."trad_action_local"("created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives",
-                                         "assigned_by", "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue")
-VALUES ('2023-12-19 10:41:20', NULL, NULL, 5060, 8, '[{"url": "http://xszhdmp.qiniuoss.xshz.vip/1702953695380.png", "name": "2023110301立案通知.png"}]', 2535,
-        '{"department_id": "401", "department_name": "南京市市场监督管理局", "name": "周晓杰", "id": "2377"}',
-        2384, '{"department_id": "403", "department_name": "玄武区市场监督管理局", "name": "万华", "id": "2382"}', '', 'f', NULL);
-
-INSERT INTO "public"."xianxia_action_local" ( "created_time", "updated_time", "updated_by", "conn_id", "dispatch_status", "archives", "assigned_by",
-                                              "assigned_by_info", "accepted_by", "accepted_by_info", "note", "supervise", "clue" )VALUES
-    ( '2023-09-28T11:12:33', NULL, NULL, 4945, 8, 'null', 2377, '{"department_id":"401","department_name":"南京市市场监督管理局","name":"周晓杰","id":"2377"}', ,
-      '{}', '', 'f', NULL );
+INSERT INTO "public"."trad_illegal"("created_at", "edition_id",
+"length", "position", "illegal_at", "channel_id",
+"created_by", "amount", "original_illegal_id", "type", "source_id") VALUES
+('2023-04-14 11:07:18.601264+08', 109015,
+ 1740, NULL, '2023-03-15 14:30:28+08', 3213,
+ 2535, 1, NULL, 2, NULL);
